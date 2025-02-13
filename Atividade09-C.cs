b720155a5576153374fd0225b4ce8c4697804245
Caio <168545443+CaioUgo@users.noreply.github.com>
2025-02-13 09:18:22 -0300

    Atividade09-C.cs

---
+++ Atividade09-C.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main()
+	{
+		
+		Console.WriteLine("Digite o tamanho da lista: ");
+		int tamanho = int.Parse(Console.ReadLine());
+		Console.WriteLine();
+		
+		List<int> inteiros = new List<int>();
+
+			for (int i = 0; i < tamanho; i++)
+			{
+			Console.WriteLine("Item {0}: ",i+1 );
+			int entrada = int.Parse(Console.ReadLine());
+			inteiros.Add(entrada);
+			}
+			Console.Clear();
+
+		
+		Console.WriteLine("Os elementos da lista são: ");
+		
+		int maior = inteiros[0];
+			
+		foreach (int numero in inteiros)
+		{
+			if (numero > maior)
+			{
+				maior = numero;
+			}
+		}
+		
+		Console.WriteLine("\n\rO maior número da lista é {0}.", maior);
+		Console.WriteLine("\n\rPressione qualquer tecla para encerrar.....");
+		Console.ReadKey(true);
+			
+	}
+	
+    }
+				
